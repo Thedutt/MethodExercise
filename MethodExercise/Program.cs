03126fb3b8fbb5ad1e8e430552c930e3a61c6395
John Dutton <johnmdutton1@gmail.com>
2020-08-24 11:16:32 -0500

    first version of MethodExercise

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int age;
+            string name;
+            string color;
+            string animal;
+            string band;
+            int num1;
+            int num2;
+
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is your favorite color?");
+            color = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite animal?");
+            animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            band = Console.ReadLine();
+
+            Console.WriteLine("Hello your name is " + name + " and your age is " + age);
+            Console.WriteLine("For some reason your favorite color is " + color);
+            Console.WriteLine("Even stranger is that your favorite animal is " + animal);
+            Console.WriteLine("However you saved yourself by having the favorite band " + band);
+
+            Console.WriteLine("Give me any number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give me another number to add");
+            num2 = int.Parse(Console.ReadLine());
+
+            int sum = Add(num1, num2);
+            Console.WriteLine($"Your number is {sum}");
+
+            Console.WriteLine("Lets do a multiplication now. Give me another number");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ok now give me another");
+            num2= int.Parse(Console.ReadLine());
+
+            int mult = Multiply(num1, num2);
+            Console.WriteLine($"your number is {mult}");
+
+        }
+
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
